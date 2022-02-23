088222476329ac48f4108e0cfdad607f8854f703
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-02-22 17:22:38 -0700

    update app with a shop page, collection preview component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = ({props}) =>(
-
-  <div>
-
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
@@ -18,11 +12,11 @@ function App() {
 {/*       Switch will only render the first path that fits and stop go through the remain paths.
  Route component will render the component with the path appended after the URL. exact's default is true   */}      
         <Route exact path='/' component={HomePage} /> 
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
   );
-}
+};
 
 export default App;
