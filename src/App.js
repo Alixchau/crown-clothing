7a52eddcbf93840881711bd1e001d24f66c484cf
Alix Shuk Han Chau <86642948+Alixchau@users.noreply.github.com>
2022-02-23 00:19:39 -0700

    add sign in page form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
  Route component will render the component with the path appended after the URL. exact's default is true   */}      
         <Route exact path='/' component={HomePage} /> 
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUpPage} />
       </Switch>
 
     </div>
